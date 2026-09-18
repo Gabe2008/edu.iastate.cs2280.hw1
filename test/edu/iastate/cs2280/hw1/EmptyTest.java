34d5de8cc1104f75ab2bd70ea8ad28116eefe152
Gabe2008 <gabe.vroom@gmail.com>
2026-09-18 09:30:39 -0500

    Start empty cell tests

---
+++ test/edu/iastate/cs2280/hw1/EmptyTest.java
@@ -0,0 +1,23 @@
+package edu.iastate.cs2280.hw1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmptyTest {
+
+    @Test
+    void who() {
+
+    }
+
+    @Test
+    void next() {
+
+    }
+
+    @Test
+    void testToString() {
+
+    }
+}
